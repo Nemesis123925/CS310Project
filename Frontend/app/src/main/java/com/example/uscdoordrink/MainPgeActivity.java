c3fb756cd9e5f332ae2f45b7b24270ea6fa8ed48
Celia Marino <celiamar@usc.edu>
2022-03-23 18:23:42 -0700

    Login screen add

---
+++ Frontend/app/src/main/java/com/example/uscdoordrink/MainPgeActivity.java
@@ -0,0 +1,14 @@
+package com.example.uscdoordrink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPgeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_pge);
+    }
+}
